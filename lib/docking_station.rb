0e68206a5965d53ea80c3214ca91f2e46cb6d608
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-03-14 15:44:38 +0000

    Updated docking_station_spec.rb syntax

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,4 @@
 class DockingStation
   def release_bike
-    'bike'
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,22 @@
 require 'docking_station'
 
 describe DockingStation do
-  it "releases 'bike' when the release_bike method is called upon it" do
-    docking = DockingStation.new # Instance of class needs to be created before running test
-    expect(docking.release_bike).to eq 'bike'
-  end
+  it "responds to release_bike" do #New instance to respond to release_bike method.
+    expect(subject).to respond_to :release_bike #Expect the 'subject' i.e. instance, to respond to the release_bike method.
 end
+end
+
+=begin
+
+Shorter option:
+describe DockingStation do
+it { is_expected.to respond_to :release_bike}
+end
+
+#respond_to => The instance and method are connected.
+#is_expected => What we are expecting to see in the programme.
+#.to => To do something.
+#:release_bike => A method.
 
 
 #Note: Always run rspec from the project directory, not in spec or lib.
@@ -18,3 +29,5 @@ end
 
 #in IRB, the path of the required file is neccessary e.g. ./lib/DockingStation
 # Rspec automatically tells Ruby to look in the /lib directory
+
+=end
