83eee9a091bc88b9780a48e99214b37ef74393dc
Magnus Holm <mail@magnusholm.com>
2017-03-16 11:52:19 +0000

    Added constant DEFAULT_CAPACITY

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,6 +2,7 @@ require_relative 'bike'
 
 class DockingStation
 attr_reader :bikes
+DEFAULT_CAPACITY = 20
 
   def initialize
     @bikes = []
@@ -21,7 +22,7 @@ attr_reader :bikes
   private
 
   def full?
-    @bikes.length >= 20
+    @bikes.length >= DEFAULT_CAPACITY
   end
 
   def empty?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -31,7 +31,7 @@ end
   end
 
   it "gives error message when user tries to dock bike and docking station is full" do
-    20.times { subject.dock Bike.new }
+    DockingStation::DEFAULT_CAPACITY.times { subject.dock Bike.new }
     expect { subject.dock Bike.new }.to raise_error("Docking station full")
   end
 
